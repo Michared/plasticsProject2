347adeb03394e5172c8e506b333364f1429645af
Thijs van Wijnen <thijsvanwijnen@hotmail.nl>
2022-03-09 12:24:20 +0100

    test

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a  Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Imports
+from mesa.visualization.ModularVisualization import ModularServer
+
+from visualize import visualize_model
+from model import Festival
+
+# Run (press the green button in the gutter to run the script)
+if __name__ == '__main__':
+    model = Festival()
+    for i in range(15):
+        model.step()
+    visualize_model(model)
