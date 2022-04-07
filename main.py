3647407c03ad86d9408f6e7b2ce47ac5b159e34b
MichaTU <m.arnoldus@student.tudelft.nl>
2022-04-07 15:33:56 +0200

    Reused cups worden bijgehouden en gevisualiseerd

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from model import Festival
 
 # Run (press the green button in the gutter to run the script)
 if __name__ == '__main__':
-    model = Festival()
+    model = Festival(drinks_for_cup=1)
     for i in range(200):
         model.step()
     visualize_model(model)
